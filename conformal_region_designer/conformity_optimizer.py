63ac63002ca19c9b7aeed4d621778e0ff6156bbe
Nandan Tumu <nandan.t@live.com>
2023-11-26 19:50:35 +0000

    Multi-timestep investigation

--- conformal_region_designer/conformity_optimizer.py
+++ conformal_region_designer/conformity_optimizer.py
@@ -2,6 +2,7 @@
 This file contains an implementation of the overall orchestrator that creates regions.
 
 """
+from copy import deepcopy
 from typing import Union
 import numpy as np
 
@@ -13,7 +14,7 @@ class ConformalRegion:
     def __init__(
         self, 
         de: Union[DensityEstimator, str] = 'kde', 
-        cl: Union[Clustering, str] = 'mean_shift', 
+        cl: Union[Clustering, str] = 'meanshift', 
         st: Union[type[ShapeTemplate], str] = 'hyperrectangle', 
         delta=0.95
     ) -> None:
@@ -25,9 +26,9 @@ class ConformalRegion:
                 raise ValueError(f"Unknown density estimator {de}")
         self.de = de
         if isinstance(cl, str):
-            if cl == 'mean_shift':
-                from .clustering import MeanShift
-                cl = MeanShift()
+            if cl == 'meanshift':
+                from .clustering import MeanShiftClustering
+                cl = MeanShiftClustering()
             else:
                 raise ValueError(f"Unknown clustering algorithm {cl}")
         self.cl = cl
@@ -66,9 +67,9 @@ class ConformalRegion:
         # For each shape compute the max score of the points assigned to it
         self.normalizing_constant = np.ones(len(self.shapes))
         for i in range(len(self.shapes)):
-            self.normalizing_constant[i] = np.quantile(real_scores[shape_idx == i], self.delta)
-            self.normalizing_constant[i] = max(self.normalizing_constant[i], 1e-10) # Prevent division by zero
+            self.normalizing_constant[i] = np.quantile(real_scores[shape_idx == i], self.delta)    
         try:
+            self.normalizing_constant += np.min(self.normalizing_constant) + 1.0
             self.normalizing_constant/= np.sum(self.normalizing_constant)
         except:
             self.normalizing_constant = np.ones(len(self.shapes))
@@ -82,6 +83,9 @@ class ConformalRegion:
         shape_idx = np.argmin(scores, axis=0)
         target_score = np.quantile(real_scores, conf_delta)
         if debug: print(f"Target score: {target_score}")
+        self.adjust_shapes(target_score)
+
+    def adjust_shapes(self, target_score: float):
         for i, shape in enumerate(self.shapes):
             shape.adjust_shape(target_score/self.normalizing_constant[i])
 
@@ -93,3 +97,65 @@ class ConformalRegion:
     
     def volume(self):
         return np.sum([shape.volume() for shape in self.shapes])
+
+
+class ConformalRegionTimeSeries(ConformalRegion):
+    """
+    This class is a wrapper around ConformalRegion that handles time series data.
+    It does so by fitting a ConformalRegion to each timestep of the data, and then
+    using a normalizing constant over each of the timesteps to achieve timeseries regions
+    """
+    def __init__(
+        self, 
+        timesteps: int,
+        de: Union[DensityEstimator, str] = 'kde', 
+        cl: Union[Clustering, str] = 'meanshift', 
+        st: Union[type[ShapeTemplate], str] = 'hyperrectangle', 
+        delta=0.95
+    ) -> None:
+        self.cregions = [ConformalRegion(de, cl, st, delta) for _ in range(timesteps)]
+        self.timesteps = timesteps
+        self.delta = delta
+
+    def fit(self, Z_train: np.ndarray, verbose: bool=False):
+        for i, cregion in enumerate(self.cregions):
+            if verbose: print(f"Fitting timestep {i}")
+            cregion.fit(Z_train[:, i].reshape(Z_train.shape[0], -1), verbose=verbose)
+        scores = np.zeros((Z_train.shape[0], self.timesteps))
+        for i, cregion in enumerate(self.cregions):
+            scores[:, i] = cregion.calculate_scores(Z_train[:, i].reshape(Z_train.shape[0], -1))
+        real_scores = np.min(scores, axis=1)
+        ts_idx = np.argmin(scores, axis=1)
+        self.normalizing_constant = np.ones(self.timesteps)
+        for i in range(self.timesteps):
+            self.normalizing_constant[i] = np.quantile(real_scores[ts_idx == i], self.delta)
+        try:
+            self.normalizing_constant += np.min(self.normalizing_constant) + 1.0
+            self.normalizing_constant/= np.sum(self.normalizing_constant)
+        except:
+            self.normalizing_constant = np.ones(len(self.shapes))
+
+    def conformalize(self, Z_cal: np.ndarray, debug=False):
+        conf_delta = conformalized_quantile(len(Z_cal), self.delta)
+        scores = np.zeros((Z_cal.shape[0], self.timesteps))
+        for i, cregion in enumerate(self.cregions):
+            scores[:, i] = cregion.calculate_scores(Z_cal[:, i].reshape(Z_cal.shape[0], -1))*self.normalizing_constant[i]
+        real_scores = np.min(scores, axis=1)
+        ts_idx = np.argmin(scores, axis=1)
+        target_score = np.quantile(real_scores, conf_delta)
+        if debug: print(f"Target score: {target_score}")
+        for i, cregion in enumerate(self.cregions):
+            cregion.adjust_shapes(target_score/self.normalizing_constant[i])
+    
+    def calculate_scores(self, Z_test: np.ndarray):
+        scores = np.zeros((Z_test.shape[0], self.timesteps))
+        for i, cregion in enumerate(self.cregions):
+            scores[:, i] = cregion.calculate_scores(Z_test[:, i].reshape(Z_test.shape[0], -1))*self.normalizing_constant[i]
+        return np.min(scores, axis=1)
+
+    def volume(self):
+        return np.sum([cregion.volume() for cregion in self.cregions])
+    
+    @property
+    def shapes(self):
+        return sum([cregion.shapes for cregion in self.cregions])
--- conformal_region_designer/density_estimation.py
+++ conformal_region_designer/density_estimation.py
@@ -1,15 +1,18 @@
+import time
 import numpy as np
 from sklearn.neighbors import KernelDensity
+from typing import Union, List
 
 from .core import DensityEstimator
 
 
 class KDE(DensityEstimator):
-    def __init__(self, bandwidth="scott", kernel="gaussian", grid_size=100):
+    def __init__(self, bandwidth="scott", kernel="gaussian", grid_size=100, bw_factor=1.0):
         self.bandwidth = bandwidth
         self.kernel = kernel
         self.kde = None
         self.grid_size = grid_size
+        self.bw_factor = bw_factor
 
     def fit(self, X, delta):
         """
@@ -20,27 +23,21 @@ class KDE(DensityEstimator):
         """
         if self.bandwidth == "scott":
             self.bandwidth = X.shape[0] ** (-1 / (X.shape[1] + 4))
-            self.bandwidth /= 3
+            self.bandwidth *= self.bw_factor
         elif self.bandwidth == "silverman":
             self.bandwidth = (X.shape[0] * (X.shape[1] + 2) / 4) ** (
                     -1 / (X.shape[1] + 4)
             )
-            self.bandwidth /= 3
+            self.bandwidth *= self.bw_factor
         self.kde = KernelDensity(bandwidth=self.bandwidth, kernel=self.kernel)
         self.kde.fit(X)
 
         self.delta = delta
 
-        # # We should also remove the outliers from the min/max
-        # self.iqr = np.quantile(X, 0.75, axis=0) - np.quantile(X, 0.25, axis=0)
-        # self.min_quantile = np.quantile(X, 0.25, axis=0) - 4 * self.iqr
-        # self.max_quantile = np.quantile(X, 0.75, axis=0) + 4 * self.iqr
-
-        # # We now need to store the range of the data
-        # self.min = np.maximum(np.min(X, axis=0), self.min_quantile)
-        # self.max = np.minimum(np.max(X, axis=0), self.max_quantile)
-        self.min = np.min(X, axis=0)
-        self.max = np.max(X, axis=0)
+        self.iqr = np.quantile(X, 0.75, axis=0) - np.quantile(X, 0.25, axis=0)
+        # Add buffer to min/max, due to bandwidth of KDE
+        self.min = np.min(X, axis=0) - 1.0 * self.iqr
+        self.max = np.max(X, axis=0) + 1.0 * self.iqr
 
     def score_samples(self, X):
         return self.kde.score_samples(X)
@@ -80,6 +77,102 @@ class KDE(DensityEstimator):
         idx = np.argmax(cum_weight >= delta)
         # Return the grid points with weight at least delta
         return grid[: idx + 1]
+
+
+# class KDE_TimeSeries(KDE):
+#     def __init__(self, 
+#                  bandwidth: Union[str, float, List[Union[str, float]]]="scott", 
+#                  kernel: Union[str, List[str]] = "gaussian", 
+#                  grid_size: Union[int, List[int]] = 100, 
+#                  bw_factor: Union[int, List[int]] = 1.0):
+#         super().__init__(bandwidth, kernel, grid_size, bw_factor)
+
+#     def _timestep_parameters(self, X):
+#         """
+#         Convert single parameters to lists of parameters for each timestep
+#         """
+#         if isinstance(self.bandwidth, float) or isinstance(self.bandwidth, str):
+#             self.bandwidth = [self.bandwidth] * X.shape[1]
+#         if isinstance(self.kernel, str):
+#             self.kernel = [self.kernel] * X.shape[1]
+#         if isinstance(self.grid_size, int):
+#             self.grid_size = [self.grid_size] * X.shape[1]
+#         if isinstance(self.bw_factor, int):
+#             self.bw_factor = [self.bw_factor] * X.shape[1]
+
+        
+#     def fit(self, X, delta):
+#         """
+#         Fit the density estimator to the data and save the range of the data
+#         This version of fit fits a KDE to each timestep of the data. 
+#         The weight of a point is the average of the weights in each timestep
+
+
+#         Args:
+#             X (np.ndarray): Data to fit the density estimator to, shape (n_samples, n_features)
+#         """
+#         self._timestep_parameters(X)
+#         self.kde = [None for _ in range(X.shape[1])]
+#         self.min = [None for _ in range(X.shape[1])]
+#         self.max = [None for _ in range(X.shape[1])]
+
+#         for t in range(X.shape[1]):
+#             if self.bandwidth[t] == "scott":
+#                 self.bandwidth[t] = X.shape[0] ** (-1 / (X.shape[1] + 4))
+#                 self.bandwidth[t] *= self.bw_factor[t]
+#             elif self.bandwidth[t] == "silverman":
+#                 self.bandwidth[t] = (X.shape[0] * (X.shape[1] + 2) / 4) ** (
+#                         -1 / (X.shape[1] + 4)
+#                 )
+#                 self.bandwidth[t] *= self.bw_factor[t]
+#             self.kde[t] = KernelDensity(bandwidth=self.bandwidth[t], kernel=self.kernel[t])
+#             self.kde[t].fit(X[:, t].reshape(-1, 1))
+
+#             self.iqr = np.quantile(X[:, t], 0.75) - np.quantile(X[:, t], 0.25)
+#             # Add buffer to min/max, due to bandwidth of KDE
+#             self.min[t] = np.min(X[:, t]) - 1.0 * self.iqr
+#             self.max[t] = np.max(X[:, t]) + 1.0 * self.iqr
+
+#         self.delta = delta
+
+#     def score_samples(self, X):
+#         """
+#         Calculate the score samples for each timestep
+#         """
+#         scores = np.zeros((X.shape[0], X.shape[1]))
+#         for t in range(X.shape[1]):
+#             scores[:, t] = self.kde[t].score_samples(X[:, t].reshape(-1, 1))
+#         # Average the scores across timesteps
+#         scores = np.mean(scores, axis=1)
+#         return scores
+
+#     def generate_points(self, delta) -> np.ndarray:
+#         """
+#         Generate points from the density estimator that have sum of weight at least delta
+#         """
+#         # We need to generate grid boundaries for each timestep independently, since the range of each timestep may be different
+#         # Then we need to combine the grid points back together
+#         timesteps = len(self.min)
+#         grid = [None for _ in range(timesteps)]
+#         grid_area = [None for _ in range(timesteps)]
+#         grid_density = [None for _ in range(timesteps)]
+#         grid_weight = [None for _ in range(timesteps)]
+#         sorted_indices = [None for _ in range(timesteps)]
+#         cum_weight = [None for _ in range(timesteps)]
+#         # Generate the grid for each timestep
+#         for t in range(timesteps):
+#             grid[t] = np.linspace(self.min[t], self.max[t], self.grid_size[t])
+#             grid_area[t] = (self.max[t] - self.min[t]) / self.grid_size[t]
+#             grid_density[t] = self.kde[t].score_samples(grid[t].reshape(-1, 1))
+#             grid_weight[t] = np.log(grid_area[t].astype(np.float64)) + grid_density[t].astype(np.float64)
+#             sorted_indices[t] = np.argsort(grid_weight[t])[::-1]
+#             grid[t] = grid[t][sorted_indices[t]]
+#             grid_weight[t] = grid_weight[t][sorted_indices[t]]
+#             cum_weight[t] = np.cumsum(np.exp(grid_weight[t]))
+#             cum_weight[t] /= timesteps
+
+#         # We want to find the global threshold such that the sum of weights is at least delta
+
 
 
 class NoOpDensityEstimator(DensityEstimator):
